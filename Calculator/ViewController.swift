ced9b5a315a8cf192290bfc31a1d238f5e95ec82
Aoi Sakaue <aoiue.w.bbb@gmail.com>
2016-02-11 15:42:28 +0900

    電卓アプリです。 けいさんくんが計算結果を教えてくれます。 画像はイラレで作成。

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet var label: UILabel!
+    
+    var number : Int = 0
+    
+    var number2 : Int = 0
+    
+    var operation : Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +30,108 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func select1(){
+        number = number*10 + 1
+        label.text = String(number)
+    }
+    
+    @IBAction func select2(){
+        number = number*10 + 2
+        label.text = String(number)
+    }
+    @IBAction func select3(){
+        number = number*10 + 3
+        label.text = String(number)
+    }
+    
+    @IBAction func select4(){
+        number = number*10 + 4
+        label.text = String(number)
+    }
+    
+    @IBAction func select5(){
+        number = number*10 + 5
+        label.text = String(number)
+    }
+    
+    @IBAction func select6(){
+        number = number*10 + 6
+        label.text = String(number)
+    }
+    
+    @IBAction func select7(){
+        number = number*10 + 7
+        label.text = String(number)
+    }
+    
+    @IBAction func select8(){
+        number = number*10 + 8
+        label.text = String(number)
+    }
+    
+    @IBAction func select9(){
+        number = number*10 + 9
+        label.text = String(number)
+    }
+    
+    @IBAction func select0(){
+        number = number*10 + 0
+        label.text = String(number)
+    }
+    
+    @IBAction func plus(){
+        label.text = "0"
+        
+        operation = 1
+        number2 = number
+        number = 0
+        
+    }
+    
+    @IBAction func minus(){
+        label.text = "0"
+        
+        operation = 2
+        number2 = number
+        number = 0
+    }
+    
+    @IBAction func multiply(){
+        label.text = "0"
+        
+        operation = 3
+        number2 = number
+        number = 0
+    }
+    
+    @IBAction func division(){
+        label.text = "0"
+        
+        operation = 4
+        number2 = number
+        number = 0
+    }
+    
+    @IBAction func equal(){
+        if operation == 1 {
+            label.text = String(number + number2)
+        }
+        if operation == 2 {
+            label.text = String(number - number2)
+        }
+        if operation == 3 {
+            label.text = String(number*number2)
+        }
+        if operation == 4 {
+            label.text = String(number/number2)
+        }
+        
+    }
+    
+    @IBAction func clear(){
+        number = 0
+        number2 = 0
+        label.text = String(number)
+    }
 }
 
